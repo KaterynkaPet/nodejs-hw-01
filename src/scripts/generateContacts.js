1ed35235991f3c942550532191e33f7c7a4e3521
KaterynkaPet <katerinapetrenko85@gmail.com>
2024-08-23 00:45:07 +0300

    added steps 1 and 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,14 @@
+import * as fs from "node:fs/promises";
+
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+    const contactList = JSON.parse(await fs.readFile(PATH_DB, "utf-8"));
+    const newContactList = Array(number).fill(0).map(createFakeContact);
+    contactList.push(...newContactList);
+    await fs.writeFile(PATH_DB, JSON.stringify(contactList, null, 2));
+};
 
 generateContacts(5);
